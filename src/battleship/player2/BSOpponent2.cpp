34df43af72c8bf20bef175ce09e8c69c944c8bfa
wagyourtail <wagyourtail@wagyourtail.xyz>
2022-03-30 19:12:33 -0700

    start work on player class rewrites to allow for multiplayer

---
+++ src/battleship/player2/BSOpponent2.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ruwag on 3/30/2022.
+//
+
+#include "BSOpponent2.h"
---
+++ src/battleship/player2/BSOpponent2.h
@@ -0,0 +1,30 @@
+//
+// Created by ruwag on 3/30/2022.
+//
+
+#ifndef BATTLESHIP_BSOPPONENT2_H
+#define BATTLESHIP_BSOPPONENT2_H
+
+
+#include <memory>
+#include "battleship/Battleship.h"
+
+class BSOpponent2 {
+    public:
+        // send attack to opponent
+        // ->
+        virtual void attack(int x, int y) = 0;
+        // poll opponent, returns status & cords of attack back, or nullptr if still waiting
+        // <-
+        virtual std::shared_ptr<Battleship::Status> ackDone(int& x, int& y) = 0;
+
+        // poll opponent, returns status & cords of attack back, or false if still waiting
+        // <-
+        virtual bool pollAttack(int& x, int& y) = 0;
+        // send acknowledgement of attack back to opponent with status
+        // ->
+        virtual void ackAttack(std::shared_ptr<Battleship::Status> status) = 0;
+};
+
+
+#endif //BATTLESHIP_BSOPPONENT2_H
